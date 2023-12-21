78e66aef55238bfb623cc8c2a4577a4de18190d8
shivaduth <71396105+shivaduth@users.noreply.github.com>
2023-12-21 11:46:48 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -3,13 +3,16 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
-const userRoute = require('./routes/user');
+import router from "./routes/user";
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocs = require('./openApiDocumentation/swagger.json');
 
 
 app.use(bodyParser.json());
 app.use(express.json());
-
-app.use('/', userRoute);
+app.use('/api-docs', swaggerUI.serve,swaggerUI.setup(swaggerDocs));
+app.use('/', router);
 app.listen(8080, ()=>{
     console.log("Listening the port at 8080");
 });
